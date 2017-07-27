7cc5b6ac460a1c5f29d0f3dba1727efd5dc20245
Joey <17710764656@163.com>
2017-07-27 17:44:38 +0800

    123

---
+++ 123/123/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  123
+//
+//  Created by Joey on 2017/7/27.
+//  Copyright © 2017年 https://mjoeygithub.github.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
